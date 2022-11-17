a37c840b76aa65064e98eec40144395c190bf0ed
feliperibeirodeoliveira <feliperibeirodeoliveiracg@gmail.com>
2022-11-17 07:44:53 -0300

    alteracao

--- src/main/java/br/edu/unisep/albumcup/data/repository/StickerRepository.java
+++ src/main/java/br/edu/unisep/albumcup/data/repository/StickerRepository.java
@@ -6,10 +6,21 @@ import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Repository
 @Service
 @Component
 public interface StickerRepository extends JpaRepository<Sticker, Integer> {
+
+    List<Sticker> finAll();
+
+    Sticker findById( int id);
+
+    void delete(Sticker sticker);
+
+
+    <Stic extends Sticker> Stic save(Sticker sticker);
 
 
 }
